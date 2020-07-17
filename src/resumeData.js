a3b9a2ae839d6aa21bd4bec0095c251b477aaa70
Coconoisette <68019638+Coconoisette@users.noreply.github.com>
2020-07-17 11:08:05 +0200

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Coco Noisette",
+    "role": "Artist, musician, nature lover, traveler, minimalist, degrowther, vegetarian, freedom seeker and many more.",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "  ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Changing the World one tiny step at a time, spreading art and kindness wherever I land, searching for myself through creativity and freedom.",
+    "address":"Feel free to contact me",
+  
+    "education":[
+      {
+        "UniversityName":"Oxford Seminars",
+        "specialization":"TESL/TESOL/TEFL",
+        "MonthOfPassing":"Sept 2019 - Nov 2019",
+        "Achievements":" Teacher training certification"
+      },
+      {
+        "UniversityName":"Concordia University",
+        "specialization":"Bachelor of Fine Arts",
+        "MonthOfPassing":"Sept 2014 - Jan 2018",
+        "Achievements":"Majored in Studio Arts"
+      },
+      {
+        "UniversityName":"Tongji University",
+        "specialization":"Exchange in Media Arts",
+        "MonthOfPassing":"Sept 2016 - May 2017",
+        "Achievements":"Minored in Media Arts"
+      }
+    ],
+    "experience":[
+      {
+        "CompanyName":"Ekkharthof, Lengwill, Switzerland",
+        "specialization":"Art Therapist Assistant",
+        "MonthOfLeaving":"Feb 2020 - present",
+        "Achievements":"Internship in Art Therapy"
+      },
+      {
+        "CompanyName":"Centro Cultural Garay Otavo, Puerto Lopez, Colombia",
+        "specialization":"English and Art Instructor",
+        "MonthOfLeaving":"Juin 2019 - August 2019",
+        "Achievements":"Taught students aged 3 to adults in classes of 5-18"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Adobe Premiere, Hitfilm"
+      },
+      {
+        "skillname":"Html, CSS, Scratch"
+      },
+      {
+        "skillname":"Photoshop"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "Backpacking":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
